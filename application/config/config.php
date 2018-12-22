745f25377a6e700214d48542ae79dd797162ba72
Vera Firmansyah <verafirmansyah@gmail.com>
2018-12-22 07:54:54 +0700

    ht

--- application/config/config.php
+++ application/config/config.php
@@ -59,7 +59,10 @@ Options +FollowSymLinks
 
 */
 
-$config['base_url']	= "http://".$_SERVER["SERVER_NAME"].":8087/yoka";
+$http = 'http' . ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 's' : '') . '://';
+$newurl = str_replace("index.php","", $_SERVER['SCRIPT_NAME']);
+$config['base_url'] = "$http" . $_SERVER['SERVER_NAME'] . "" . $newurl;
+
 
 /*
 |--------------------------------------------------------------------------
